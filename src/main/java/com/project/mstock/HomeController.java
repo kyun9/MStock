96a6e42001c94afe171211195243b01ccf791062
hyun kyung <gusl108@naver.com>
2019-09-12 00:52:37 +0900

    prepare templete

---
+++ src/main/java/com/project/mstock/HomeController.java
@@ -0,0 +1,17 @@
+package com.project.mstock;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("home");
+		return mav;
+	}
+	
+}
