1debf52c6c9e2e058e93fd73f5a70c20f7e6a2f6
minwooDB <alsdn114560@gmail.com>
2019-09-20 17:23:28 +0900

    금융뉴스크롤링
    금융뉴스크롤링

---
+++ r_data/금융뉴스 크롤링/new_for.R
@@ -0,0 +1,28 @@
+for(i in 1:20) {
+  titleCSS <- paste('body > div > table.type5 > tbody > tr:nth-child(',i,') > td.title > a', sep='')
+  title <- remDr$findElements(using='css', titleCSS)
+  title_text <- sapply(title ,function(x){x$getElementText()}) 
+  title2 <- unlist(title_text)
+  if(is.null(title2) == FALSE){
+    title_url <- unlist(sapply(title,function(x){x$getElementAttribute('href')}))
+    
+    title_infoCSS <- paste('body > div > table.type5 > tbody >  tr:nth-child(',i,') > td.info', sep='')
+    title_info<-remDr$findElements(using='css', title_infoCSS)
+    info <-sapply(title_info,function(x){x$getElementText()})
+    info2 <- unlist(info)
+    
+    title_timeCSS <- paste('body > div > table.type5 > tbody > tr:nth-child(',i,') > td.date', sep='')
+    title_time<-remDr$findElements(using='css',title_timeCSS)
+    time <-sapply(title_time,function(x){x$getElementText()})
+    time2 <- unlist(time)
+    
+    gotonews <- read_html(title_url,encoding="EUC-KR")
+    nodes <- html_nodes(gotonews, "#news_read")
+    newstext <- html_text(nodes)
+    newstext<- gsub("\t", "", newstext)
+    newstext<- gsub("\n", "", newstext)
+    page <-data.frame(title2, info2, time2)
+    navereco_info <- rbind(navereco_info,page)
+    navereconomic_contents<-rbind( navereconomic_contents,newstext)
+  }
+}
