b48a828eb4339b18c66dad9953f7fb12df9e51fb
kyun9 <gusl108@naver.com>
2019-09-24 14:37:45 +0900

    gson 추가하여 json파일 생성

--- src/main/java/service/StockInfoScheduler.java
+++ src/main/java/service/StockInfoScheduler.java
@@ -1,9 +1,15 @@
 package service;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import dao.CompanyDAO;
 import dao.StockInfoDAO;
@@ -17,34 +23,55 @@ public class StockInfoScheduler {
 	StockInfoDAO stockinfoDAO;
 	@Autowired
 	CompanyDAO companyDAO;
-	static String[] stockInfos= {"006400","000660","012330","035420","066570","068270",
-			"090430","004170","033780","035720","010950","161890"};
-	
+	static String[] stockInfos = { "006400", "000660", "012330", "035420", "066570", "068270", "090430", "004170",
+			"055550", "035720", "010950", "161890" };
+
 //	task 꺼둠
 //	@Scheduled(cron="0/10 0 9-16 ? * MON-FRI")  // 월요일~금, 9시에서 16시,10초간격
-	@Scheduled(cron="0/10 * * * * *")  // 10초간격
-	public void scheduleRun() {
-		StockInfoVO vo= new StockInfoVO();
+//	@Scheduled(cron = "0/10 * * * * *") // 10초간격
+//	public void scheduleRun() {
+//		StockInfoVO vo = new StockInfoVO();
+//		try {
+//			for (int i = 0; i < stockInfos.length; i++) {
+//				vo = service.getInfo(stockInfos[i]);
+//				String curJuka = vo.getStockinfo()[1].replace(",", "");
+//				if (stockinfoDAO.insertStockInfo(vo.getJongCd(), curJuka, vo.getGettime())) {
+//					System.out.println("insert 완료");
+//					System.out.println(vo.getGettime());
+//				}
+//				if (companyDAO.updateCurjuka(curJuka, vo.getJongCd())) {
+//					System.out.println("company curjuka update 완료");
+//				}
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+	
+	
+	//json파일 생성
+	//@Scheduled(cron="0/10 0 9-16 ? * MON-FRI")  // 월요일~금, 9시에서 16시,10초간격
+	@Scheduled(cron = "0/10 * * * * *") // 10초간격)
+	public void scaheduleJSON() {
+		StockInfoVO vo = new StockInfoVO();
 		try {
-			for(int i=0;i<stockInfos.length;i++) {
+			for (int i = 0; i < stockInfos.length; i++) {
+				Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
 				vo = service.getInfo(stockInfos[i]);
-				String curJuka = vo.getStockinfo()[1].replace(",", "");
-				if(stockinfoDAO.insertStockInfo(vo.getJongCd(), curJuka, vo.getGettime())) {
-					System.out.println("insert 완료");
-					System.out.println(vo.getGettime());
-				}
-				if(companyDAO.updateCurjuka(curJuka, vo.getJongCd())) {
-					System.out.println("company curjuka update 완료");
-				}
+
+				String filePath = "c:/uploadtest/" + vo.getJongCd() + ".json";
+
+				BufferedWriter writer = new BufferedWriter(
+						new OutputStreamWriter(new FileOutputStream(filePath, true), "UTF-8"));
+				gson.toJson(vo, writer);
+				writer.close();
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
-	
+
 //	@Scheduled(cron="* 30 8 * * MON-FRI")   //월~금 8시 30분 stockinfos 테이블 초기화
 //	@Scheduled(cron="0/30 * * * * *")     //30초간격
 //	public void clearStockDB() {
