d2f58119ff2002bdd8453956a82b05a229af6267
kyun9 <gusl108@naver.com>
2019-09-24 15:57:03 +0900

    일정시간 json 파일 삭제기능 완료

--- src/main/java/service/StockInfoScheduler.java
+++ src/main/java/service/StockInfoScheduler.java
@@ -1,8 +1,6 @@
 package service;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -49,19 +47,21 @@ public class StockInfoScheduler {
 //	}
 	
 	
-	//json파일 생성
+	//종목 code별 json파일 생성
 	//@Scheduled(cron="0/10 0 9-16 ? * MON-FRI")  // 월요일~금, 9시에서 16시,10초간격
-	@Scheduled(cron = "0/10 * * * * *") // 10초간격)
+	@Scheduled(cron = "0/10 * * * * *") // 10초간격 test
 	public void scaheduleJSON() {
 		StockInfoVO vo = new StockInfoVO();
 		try {
 			for (int i = 0; i < stockInfos.length; i++) {
+				//json형태로 변환해줄 gson 객체 생성
 				Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
+				
+				//원하는 종목을  service에서 파싱해와서 vo객체 담고 fileName을 종목코드명을 지정
 				vo = service.getInfo(stockInfos[i]);
-
 				String filePath = "c:/uploadtest/" + vo.getJongCd() + ".json";
-
+				
+				//파일에 utf-8로 인코딩하고 json파일에 append  
 				BufferedWriter writer = new BufferedWriter(
 						new OutputStreamWriter(new FileOutputStream(filePath, true), "UTF-8"));
 				gson.toJson(vo, writer);
@@ -81,4 +81,25 @@ public class StockInfoScheduler {
 //			System.out.println("=======================");
 //		}
 //	}
+	
+	
+//	json 파일 삭제 
+//	@Scheduled(cron="* 30 8 * * MON-FRI")   //월~금 8시 30분 종목별 json파일삭제 
+	@Scheduled(cron="0/10 * * * * *")     //30초간격 test
+	public void clearStockDB() {
+		try {
+			for (int i = 0; i < stockInfos.length; i++) {
+				File file = new File("c:/uploadtest/"+stockInfos[i]+".json");
+				if( file.exists() ){
+					if(file.delete()){
+		    			System.out.println("파일삭제 성공");
+		    		}else{
+		    			System.out.println("파일삭제 실패");
+		    		}
+				}
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
